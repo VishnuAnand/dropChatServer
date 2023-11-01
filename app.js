9c4f90f2101c9fd78c06c88336ade80a60deb668
developergireesh <117424095+developergireesh@users.noreply.github.com>
2023-11-01 17:43:03 +0530

    App.js from gireesh

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ io.on("connection", (socket) => {
   socket.on("msg",(data)=>{
     console.log("msg: "+data);
   });
+  socket.on("file",(data)=>{
+    console.log("file: "+data);
+    socket.emit("file",data);
+  });
 });
 
 httpServer.listen(3000);
