e32a010721011062abb29ec39487483096dad69f
vaportrave <vishnu@portrave.com>
2023-11-02 08:10:40 +0530

    get users connected to the socket

--- app.js
+++ app.js
@@ -16,6 +16,24 @@ io.on("connection", (socket) => {
     console.log("\n------------------------------------------")
     socket.emit("file",data);
   });
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  console.log(socket.id);
+  socket.emit("users", users);
+  // notify existing users
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.username,
+  });
+  console.log(socket.userID);
+  console.log(socket.username);
 });
+
+
 
 httpServer.listen(3000);
