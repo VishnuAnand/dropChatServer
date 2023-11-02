677095df13f876b432f425521037add672f2dda7
Vishnu Anand <vishnuanand.codemazk@gmail.com>
2023-11-02 18:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 const { createServer } = require("http");
-const { Server } = require("socket.io");
+  const { Server } = require("socket.io");
+  const { Socket } = require("socket.io-client");
 
-const httpServer = createServer();
-const io = new Server(httpServer, { /* options */ });
 
-io.on("connection", (socket) => {
-  console.log('connected');
-  socket.on("msg",(data)=>{
-    console.log("msg: "+data);
-  });
-  socket.on("file",(data)=>{
-    console.log("filename: "+data['name']);
-    console.log("content:\n------------------------------------------");
-    console.log(""+data['data']);
-    console.log("\n------------------------------------------")
-    socket.emit("file",data);
-  });
-  const users = [];
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push({
-      userID: id,
-      username: socket.username,
+  const httpServer = createServer();
+  const io = new Server(httpServer, { /* options */ });
+
+  io.on("connection", (socket) => {
+    socketI=socket;
+    console.log('connected');
+    socket.on("msg",(data)=>{
+      console.log("msg: "+data);
     });
-  }
-  console.log(socket.id);
-  socket.emit("users", users);
-  // notify existing users
-  socket.broadcast.emit("user connected", {
-    userID: socket.id,
-    username: socket.username,
+    socket.on("file",(data)=>{
+      console.log("filename: "+data['name']);
+      console.log("content:
+------------------------------------------");
+      console.log(""+data['data']);
+      console.log("
+------------------------------------------")
+      socket.broadcast.emit("file",data);
+    });
+    const users = [];
+
+    // socket.on("username",(data)=>{
+    //   console.log("username",data);
+    //   users.push({
+    //     username: data,
+    //    // username: socket.handshake.auth.username,
+    //   });
+    //   console.log("usersarray",users);
+    //   socket.emit("username",users);
+    // });
+
+    socket.on("username",(data)=>{
+      console.log("username recieved",data);
+      users.push({
+            username: data,
+            socketId: socket.id,
+        });
+      socket.broadcast.emit("username",users);
+    });
+
+    // for (let [id, socket] of io.of("/").sockets) {
+    //   socket.on("username",(data)=>{
+    //     users.push({
+    //       userID: id,
+    //       username: data,
+    //     });
+    //     console.log("io.on.username",data);
+    //     socket.emit("username", users);
+    //   });
+    //   console.log("sockets:",id)
+    // }
+
+
+    console.log(users);
+
+    //console.log(socket.handshake.auth.username);
   });
-  console.log(socket.userID);
-  console.log(socket.username);
-});
 
 
 
-httpServer.listen(3000);
+
+
+
+
+  httpServer.listen(3000);
