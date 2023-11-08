7e3440166c9259469093f97e181f11f80a906caa
Vishnu Anand <vishnuanand.codemazk@gmail.com>
2023-11-08 17:37:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,83 @@
-const { createServer } = require("http");
-  const { Server } = require("socket.io");
-  const { Socket } = require("socket.io-client");
+const express = require('express');
+const mongoose = require('mongoose');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+const File = require("./models/File");
+
+const fileRoutes = require("./routes/fileRoutes");
+const { error } = require('console');
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://bijil:E3wttXF5rMM3gkYZ@cluster0.rj9atia.mongodb.net/dropchat?retryWrites=true&w=majority").then(con => {
+    console.log( "DB connection successfull" )
+}).catch(err => console.log(err.message))
 
 
-  const httpServer = createServer();
-  const io = new Server(httpServer, { /* options */ });
 
-  io.on("connection", (socket) => {
-    socketI=socket;
-    console.log('connected');
-    socket.on("msg",(data)=>{
-      console.log("msg: "+data);
-    });
-    socket.on("file",(data)=>{
-      console.log("filename: "+data['name']);
-      console.log("content:
-------------------------------------------");
-      console.log(""+data['data']);
-      console.log("
-------------------------------------------")
-      socket.broadcast.emit("file",data);
-    });
-    const users = [];
+const app = express();
 
-    // socket.on("username",(data)=>{
-    //   console.log("username",data);
-    //   users.push({
-    //     username: data,
-    //    // username: socket.handshake.auth.username,
-    //   });
-    //   console.log("usersarray",users);
-    //   socket.emit("username",users);
-    // });
+const httpServer = createServer();
+const io = new Server(httpServer, { /* options */ });
 
-    socket.on("username",(data)=>{
-      console.log("username recieved",data);
-      users.push({
-            username: data,
-            socketId: socket.id,
-        });
-      socket.broadcast.emit("username",users);
+
+var usersList = new Map();
+
+io.on("connection", (socket) => {
+
+    let event_socket_id = socket.id;
+
+    socket.on('file', async(data)=> {
+        console.log("File Recieved:", data);
+
+        hash = data.hash;
+        const file_data = await File.find({hash:hash});
+
+        console.log("Fetched from DB:", file_data);
+
+        socket.broadcast.emit("file",file_data);
     });
 
-    // for (let [id, socket] of io.of("/").sockets) {
-    //   socket.on("username",(data)=>{
-    //     users.push({
-    //       userID: id,
-    //       username: data,
-    //     });
-    //     console.log("io.on.username",data);
-    //     socket.emit("username", users);
-    //   });
-    //   console.log("sockets:",id)
-    // }
+    //User connected with username
+    socket.on('username',(data)=>{
+
+        usersList.set(event_socket_id,data);
+        let usersObj = Object.fromEntries(usersList);
+
+        console.log('User connected',data);
+
+        //var usersWithoutSender= usersList;
+        //usersWithoutSender.delete(event_socket_id);
+
+        console.log('User connected user list',usersList);
+
+        socket.emit("username",usersObj);
+        socket.broadcast.emit("username",usersObj);
+
+    });
+
+    socket.on("disconnect", (reason) => {
+        usersList.delete(event_socket_id);
+        console.log("User disconnected new users list: ",usersList);
+
+        let usersObj = Object.fromEntries(usersList);
+
+        socket.emit("username",usersObj);
+        socket.broadcast.emit("username",usersObj);
+
+      });
+})
 
 
-    console.log(users);
-
-    //console.log(socket.handshake.auth.username);
-  });
+app.use("/api",fileRoutes);
 
 
+const port = 3001;
+app.listen(port,() => {
+    console.log(`Server running on port:${port}`);
+})
+
+httpServer.listen(3000,() => {
+    console.log(`Socket Server running on port:3000`);
+})
 
 
-
-
-
-  httpServer.listen(3000);
